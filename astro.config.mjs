74c3b2ac53508edd01008e4cda02917dd748be2d
HanBlack <dvin9552@gmail.com>
2024-03-23 21:29:39 +0000

    pushtoweb

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,19 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import astroI18next from "astro-i18next";
 
-// https://astro.build/config
+
+const SERVER_PORT = 3000;
+const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
+const LIVE_URL = "https://rhodenny.github.io"
+const SCRIPT = process.env.npm_life_cycle_scipt || "";
+const isBuild = SCRIPT.includes("astro build");
+let BASE_URL = LOCALHOST_URL;
+if (isBuild) {
+  BASE_URL = LIVE_URL;
+}
 export default defineConfig({
-  integrations: [astroI18next(), tailwind()]
+  integrations: [astroI18next(), tailwind()],
+  site: BASE_URL,
+  server: {port: SERVER_PORT},
 
 });
