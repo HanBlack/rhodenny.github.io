3cf68c69756ab615e3bc10365a0b69112f33219c
HanBlack <dvin9552@gmail.com>
2024-03-24 07:10:08 +0000

    adding global

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  integrations: [astroI18next(), tailwind()],
+  integrations: [ tailwind()],
   site: "https://hanblack.github.io/rhodenny.github.io",
   base: '/rhodenny.github.io/',
 });
