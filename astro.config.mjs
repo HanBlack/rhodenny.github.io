617f89e690450b792f6e12ccb0f5d50220c86985
HanBlack <dvin9552@gmail.com>
2024-03-24 16:58:40 +0000

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  integrations: [ tailwind()],
+  integrations: [ tailwind(), sitemap()],
   site: "https://hanblack.github.io/rhodenny.github.io",
   base: '/rhodenny.github.io/',
 });
