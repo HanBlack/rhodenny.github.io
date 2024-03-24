1b0f681b16f28037b83ede01344bd2ff90f1479d
HanBlack <dvin9552@gmail.com>
2024-03-24 07:06:35 +0000

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [astroI18next(), tailwind()],
   site: "https://hanblack.github.io/rhodenny.github.io",
-  base: '/rhodenny.github.io'
+  base: '/rhodenny.github.io/',
 });
